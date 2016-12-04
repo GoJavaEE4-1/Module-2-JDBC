e12504fb511c517ba9fccd94c96201102ffebf6a
DGreenleaf <serg.savitskiy@gmail.com>
2016-12-04 23:59:52 +0200

    adding DAO and Impl

---
+++ src/model/dao/CompaniesDAO.java
@@ -0,0 +1,26 @@
+package model.dao;
+
+import model.entity.Company;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+/**
+ * Created by Sergiy on 12/4/16.
+ */
+
+//Create CompaniesDAO interface that implements DAO
+//        and also add methods specific for the companies table:
+//        findByName(String name). Create CompaniesDAOImpl class
+//which implements CompaniesDAO interface and implements all methods using JDBC.
+
+
+public interface CompaniesDAO<T> implements DAO<T> {
+
+    Company get (long id)
+            throws SQLException;
+
+    Collection<Company> findByName(String name)
+            throws SQLException;
+
+}
---
+++ src/model/dao/CompaniesDAOImpl.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Created by Sergiy on 12/4/16.
+ */
+public class CompaniesDAOImpl implements CompaniesDAO {
+
+    Connection connection = DriverManager.getConnection()
+
+}
+
+
