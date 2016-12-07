9aecebb7d69dde379f6982ec6ae10b2d782d4f11
art07 <artem-dubinin@yandex.ru>
2016-12-08 01:59:40 +0200

    DeveloperDAOImpl+SkillDAOImpl (Updated)

--- src/model/dao/DeveloperDAOImpl.java
+++ src/model/dao/DeveloperDAOImpl.java
@@ -1,244 +1,137 @@
-package model.dao;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+package java_ee_module_2;
 import java.sql.SQLException;
 
 class DeveloperDAOImpl implements DeveloperDAO {
-	static final String JDBC_DRIVER = "org.postgresql.Driver";
-	static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/goit";
-	static final String USER = "postgres";
-	static final String PASSWORD = "superuser";
-	
+	private ConnectionUtility connectionUtility = new ConnectionUtility();
+
 	@Override
 	public void create(Developer t) {
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
 		try{
-			Class.forName(JDBC_DRIVER);
-			connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
-			String sql = "insert into m2.developers (developer_id, developer_first_name, developer_last_name, developer_email, developer_phone, project_id, company_id) "
+			String sql = "insert into developers (developer_id, developer_first_name, developer_last_name, developer_email, developer_phone, project_id, company_id) "
 						+ "values (?, ?, ?, ?, ?, ?, ?)";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setInt(1, t.getDeveloperId());
-			preparedStatement.setString(2, t.getDeveloperFirstName());
-			preparedStatement.setString(3, t.getDeveloperLastName());
-			preparedStatement.setString(4, t.getDeveloperEmail());
-			preparedStatement.setString(5, t.getDeveloperPhone());
-			preparedStatement.setInt(6, t.getProject().getProjectId());
-			preparedStatement.setInt(7, t.getCompany().getCompanyId());
-	        preparedStatement.executeUpdate();
+			connectionUtility.setPreparedStatement(connectionUtility.getConnection().prepareStatement(sql));
+			connectionUtility.getPreparedStatement().setInt(1, t.getDeveloperId());
+			connectionUtility.getPreparedStatement().setString(2, t.getDeveloperFirstName());
+			connectionUtility.getPreparedStatement().setString(3, t.getDeveloperLastName());
+			connectionUtility.getPreparedStatement().setString(4, t.getDeveloperEmail());
+			connectionUtility.getPreparedStatement().setString(5, t.getDeveloperPhone());
+			connectionUtility.getPreparedStatement().setInt(6, t.getProject().getProjectId());
+			connectionUtility.getPreparedStatement().setInt(7, t.getCompany().getCompanyId());
+			connectionUtility.getPreparedStatement().executeUpdate();
             System.out.println("Successfully added!");
-		} catch (ClassNotFoundException | SQLException e){
-			System.out.println("Bug!");
-		} finally {
-			try {
-				if(connection != null) connection.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(preparedStatement != null) preparedStatement.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(resultSet != null) resultSet.close();
-			} catch (SQLException e) {e.printStackTrace();}
-		}
+		} catch (SQLException e){e.printStackTrace();
+		} finally {connectionUtility.closeConnection();}
 	}
 	
 	@Override
 	public Developer get(int id) {
 		Developer d = new Developer();
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
 		try{
-			Class.forName(JDBC_DRIVER);
-			connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
-			String sql = "select m2.developers.developer_id, m2.developers.developer_first_name, m2.developers.developer_last_name, m2.developers.developer_email, "
-						+ "m2.developers.developer_phone, m2.developers.project_id, m2.projects.project_name, m2.developers.company_id, m2.companies.company_name "
-						+ "from m2.developers "
-						+ "inner join m2.projects on m2.developers.project_id = m2.projects.project_id "
-						+ "inner join m2.companies on m2.developers.company_id = m2.companies.company_id "
+			String sql = "select developers.developer_id, developers.developer_first_name, developers.developer_last_name, developers.developer_email, "
+						+ "developers.developer_phone, developers.project_id, projects.project_name, developers.company_id, companies.company_name "
+						+ "from developers "
+						+ "inner join projects on developers.project_id = projects.project_id "
+						+ "inner join companies on developers.company_id = companies.company_id "
 						+ "where developer_id = ?";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setInt(1, id);
-			resultSet = preparedStatement.executeQuery();
-			while (resultSet.next()) {
-				d.setDeveloperId(resultSet.getInt("developer_id"));
-				d.setDeveloperFirstName(resultSet.getString("developer_first_name"));
-				d.setDeveloperLastName(resultSet.getString("developer_last_name"));
-				d.setDeveloperEmail(resultSet.getString("developer_email"));
-				d.setDeveloperPhone(resultSet.getString("developer_phone"));
-				d.setProject(new Project(resultSet.getInt("project_id"), resultSet.getString("project_name")));
-				d.setCompany(new Company(resultSet.getInt("company_id"), resultSet.getString("company_name")));
+			connectionUtility.setPreparedStatement(connectionUtility.getConnection().prepareStatement(sql));
+			connectionUtility.getPreparedStatement().setInt(1, id);
+			connectionUtility.setResultSet(connectionUtility.getPreparedStatement().executeQuery());
+			while (connectionUtility.getResultSet().next()) {
+				d.setDeveloperId(connectionUtility.getResultSet().getInt("developer_id"));
+				d.setDeveloperFirstName(connectionUtility.getResultSet().getString("developer_first_name"));
+				d.setDeveloperLastName(connectionUtility.getResultSet().getString("developer_last_name"));
+				d.setDeveloperEmail(connectionUtility.getResultSet().getString("developer_email"));
+				d.setDeveloperPhone(connectionUtility.getResultSet().getString("developer_phone"));
+				d.setProject(new Project(connectionUtility.getResultSet().getInt("project_id"), connectionUtility.getResultSet().getString("project_name")));
+				d.setCompany(new Company(connectionUtility.getResultSet().getInt("company_id"), connectionUtility.getResultSet().getString("company_name")));
 			}
-		} catch (ClassNotFoundException | SQLException e){
-			e.printStackTrace();
-			System.out.println("Bug!");
-		} finally {
-			try {
-				if(connection != null) connection.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(preparedStatement != null) preparedStatement.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(resultSet != null) resultSet.close();
-			} catch (SQLException e) {e.printStackTrace();}
-		}
+		} catch (SQLException e){e.printStackTrace();
+		} finally {connectionUtility.closeConnection();}
 		return d;
 	}
 	
 	@Override
 	public void update(Developer t) {
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
 		try{
-			Class.forName(JDBC_DRIVER);
-			connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
-			String sql = "update m2.developers set developer_first_name=?, developer_last_name=?, developer_email=?, developer_phone=?, project_id=?, company_id=? where developer_id=?";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setString(1, "ValentinOOO");
-			preparedStatement.setString(2, t.getDeveloperLastName());
-			preparedStatement.setString(3, t.getDeveloperEmail());
-			preparedStatement.setString(4, t.getDeveloperPhone());
-			preparedStatement.setInt(5, t.getProject().getProjectId());
-			preparedStatement.setInt(6, t.getCompany().getCompanyId());
-			preparedStatement.setInt(7, t.getDeveloperId());
-            preparedStatement.executeUpdate();
+			String sql = "update developers set developer_first_name=?, developer_last_name=?, developer_email=?, developer_phone=?, project_id=?, company_id=? where developer_id=?";
+			connectionUtility.setPreparedStatement(connectionUtility.getConnection().prepareStatement(sql));
+			connectionUtility.getPreparedStatement().setString(1, "Valentin");
+			connectionUtility.getPreparedStatement().setString(2, t.getDeveloperLastName());
+			connectionUtility.getPreparedStatement().setString(3, t.getDeveloperEmail());
+			connectionUtility.getPreparedStatement().setString(4, t.getDeveloperPhone());
+			connectionUtility.getPreparedStatement().setInt(5, t.getProject().getProjectId());
+			connectionUtility.getPreparedStatement().setInt(6, t.getCompany().getCompanyId());
+			connectionUtility.getPreparedStatement().setInt(7, t.getDeveloperId());
+			connectionUtility.getPreparedStatement().executeUpdate();
             System.out.println("Seccessfully updated!");
-		} catch (ClassNotFoundException | SQLException e){
-			e.printStackTrace();
-			System.out.println("Bug!");
-		} finally {
-			try {
-				if(connection != null) connection.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(preparedStatement != null) preparedStatement.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(resultSet != null) resultSet.close();
-			} catch (SQLException e) {e.printStackTrace();}
-		}
+		} catch (SQLException e){e.printStackTrace();
+		} finally {connectionUtility.closeConnection();}
 	}
 	
 	@Override
 	public void delete(int id) {
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
 		try{
-			Class.forName(JDBC_DRIVER);
-			connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
-			String sql = "delete from m2.developers where developer_id = ?";
-			preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setInt(1, id);
-			preparedStatement.executeUpdate();
+			String sql = "delete from developers where developer_id = ?";
+			connectionUtility.setPreparedStatement(connectionUtility.getConnection().prepareStatement(sql));
+			connectionUtility.getPreparedStatement().setInt(1, id);
+			connectionUtility.getPreparedStatement().executeUpdate();
             System.out.println("Successfully deleted!");
-		} catch (ClassNotFoundException | SQLException e){
-			e.printStackTrace();
-			System.out.println("Bug!");
-		} finally {
-			try {
-				if(connection != null) connection.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(preparedStatement != null) preparedStatement.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(resultSet != null) resultSet.close();
-			} catch (SQLException e) {e.printStackTrace();}
-		}
+		} catch (SQLException e){e.printStackTrace();
+		} finally {connectionUtility.closeConnection();}
 	}
 	
 	@Override
 	public Developer findByName(String name) {
 		Developer d = new Developer();
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
 		try{
-			Class.forName(JDBC_DRIVER);
-			connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
-			String sql = "select m2.developers.developer_id, m2.developers.developer_first_name, m2.developers.developer_last_name, m2.developers.developer_email, "
-						+ "m2.developers.developer_phone, m2.developers.project_id, m2.projects.project_name, m2.developers.company_id, m2.companies.company_name "
-						+ "from m2.developers "
-						+ "inner join m2.projects on m2.developers.project_id = m2.projects.project_id "
-						+ "inner join m2.companies on m2.developers.company_id = m2.companies.company_id "
+			String sql = "select developers.developer_id, developers.developer_first_name, developers.developer_last_name, developers.developer_email, "
+						+ "developers.developer_phone, developers.project_id, projects.project_name, developers.company_id, companies.company_name "
+						+ "from developers "
+						+ "inner join projects on developers.project_id = projects.project_id "
+						+ "inner join companies on developers.company_id = companies.company_id "
 						+ "where developer_first_name = ?";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setString(1, name);
-			resultSet = preparedStatement.executeQuery();
-			while (resultSet.next()) {
-				d.setDeveloperId(resultSet.getInt("developer_id"));
-				d.setDeveloperFirstName(resultSet.getString("developer_first_name"));
-				d.setDeveloperLastName(resultSet.getString("developer_last_name"));
-				d.setDeveloperEmail(resultSet.getString("developer_email"));
-				d.setDeveloperPhone(resultSet.getString("developer_phone"));
-				d.setProject(new Project(resultSet.getInt("project_id"), resultSet.getString("project_name")));
-				d.setCompany(new Company(resultSet.getInt("company_id"), resultSet.getString("company_name")));
+			connectionUtility.setPreparedStatement(connectionUtility.getConnection().prepareStatement(sql));
+			connectionUtility.getPreparedStatement().setString(1, name);
+			connectionUtility.setResultSet(connectionUtility.getPreparedStatement().executeQuery());
+			while (connectionUtility.getResultSet().next()) {
+				d.setDeveloperId(connectionUtility.getResultSet().getInt("developer_id"));
+				d.setDeveloperFirstName(connectionUtility.getResultSet().getString("developer_first_name"));
+				d.setDeveloperLastName(connectionUtility.getResultSet().getString("developer_last_name"));
+				d.setDeveloperEmail(connectionUtility.getResultSet().getString("developer_email"));
+				d.setDeveloperPhone(connectionUtility.getResultSet().getString("developer_phone"));
+				d.setProject(new Project(connectionUtility.getResultSet().getInt("project_id"), connectionUtility.getResultSet().getString("project_name")));
+				d.setCompany(new Company(connectionUtility.getResultSet().getInt("company_id"), connectionUtility.getResultSet().getString("company_name")));
 			}
-		} catch (ClassNotFoundException | SQLException e){
-			e.printStackTrace();
-			System.out.println("Bug!");
-		} finally {
-			try {
-				if(connection != null) connection.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(preparedStatement != null) preparedStatement.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(resultSet != null) resultSet.close();
-			} catch (SQLException e) {e.printStackTrace();}
-		}
+		} catch (SQLException e){e.printStackTrace();
+		} finally {connectionUtility.closeConnection();}
 		return d;
 	}
+	
 	@Override
 	public Developer findByFullName(String firstName, String lastName) {
 		Developer d = new Developer();
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
 		try{
-			Class.forName(JDBC_DRIVER);
-			connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
-			String sql = "select m2.developers.developer_id, m2.developers.developer_first_name, m2.developers.developer_last_name, m2.developers.developer_email, "
-						+ "m2.developers.developer_phone, m2.developers.project_id, m2.projects.project_name, m2.developers.company_id, m2.companies.company_name "
-						+ "from m2.developers "
-						+ "inner join m2.projects on m2.developers.project_id = m2.projects.project_id "
-						+ "inner join m2.companies on m2.developers.company_id = m2.companies.company_id "
+			String sql = "select developers.developer_id, developers.developer_first_name, developers.developer_last_name, developers.developer_email, "
+						+ "developers.developer_phone, developers.project_id, projects.project_name, developers.company_id, companies.company_name "
+						+ "from developers "
+						+ "inner join projects on developers.project_id = projects.project_id "
+						+ "inner join companies on developers.company_id = companies.company_id "
 						+ "where developer_first_name = ? and developer_last_name = ?";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setString(1, firstName);
-			preparedStatement.setString(2, lastName);
-			resultSet = preparedStatement.executeQuery();
-			while (resultSet.next()) {
-				d.setDeveloperId(resultSet.getInt("developer_id"));
-				d.setDeveloperFirstName(resultSet.getString("developer_first_name"));
-				d.setDeveloperLastName(resultSet.getString("developer_last_name"));
-				d.setDeveloperEmail(resultSet.getString("developer_email"));
-				d.setDeveloperPhone(resultSet.getString("developer_phone"));
-				d.setProject(new Project(resultSet.getInt("project_id"), resultSet.getString("project_name")));
-				d.setCompany(new Company(resultSet.getInt("company_id"), resultSet.getString("company_name")));
+			connectionUtility.setPreparedStatement(connectionUtility.getConnection().prepareStatement(sql));
+			connectionUtility.getPreparedStatement().setString(1, firstName);
+			connectionUtility.getPreparedStatement().setString(2, lastName);
+			connectionUtility.setResultSet(connectionUtility.getPreparedStatement().executeQuery());
+			while (connectionUtility.getResultSet().next()) {
+				d.setDeveloperId(connectionUtility.getResultSet().getInt("developer_id"));
+				d.setDeveloperFirstName(connectionUtility.getResultSet().getString("developer_first_name"));
+				d.setDeveloperLastName(connectionUtility.getResultSet().getString("developer_last_name"));
+				d.setDeveloperEmail(connectionUtility.getResultSet().getString("developer_email"));
+				d.setDeveloperPhone(connectionUtility.getResultSet().getString("developer_phone"));
+				d.setProject(new Project(connectionUtility.getResultSet().getInt("project_id"), connectionUtility.getResultSet().getString("project_name")));
+				d.setCompany(new Company(connectionUtility.getResultSet().getInt("company_id"), connectionUtility.getResultSet().getString("company_name")));
 			}
-		} catch (ClassNotFoundException | SQLException e){
-			e.printStackTrace();
-			System.out.println("Bug!");
-		} finally {
-			try {
-				if(connection != null) connection.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(preparedStatement != null) preparedStatement.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(resultSet != null) resultSet.close();
-			} catch (SQLException e) {e.printStackTrace();}
-		}
+		} catch (SQLException e){e.printStackTrace();
+		} finally {connectionUtility.closeConnection();}
 		return d;
 	}
 }
--- src/model/dao/SkillDAOImpl.java
+++ src/model/dao/SkillDAOImpl.java
@@ -1,139 +1,65 @@
-package model.dao;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+package java_ee_module_2;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 class SkillDAOImpl implements SkillDAO {
-	static final String JDBC_DRIVER = "org.postgresql.Driver";
-	static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/goit";
-	static final String USER = "postgres";
-	static final String PASSWORD = "superuser";
+	private ConnectionUtility connectionUtility = new ConnectionUtility();
 	
 	@Override
 	public void create(Skill t) {
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
 		try{
-			Class.forName(JDBC_DRIVER);
-			connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
-			String sql = "insert into m2.skills (skill_id, skill_name) values (?, ?)";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setInt(1, t.getSkillId());
-			preparedStatement.setString(2, t.getSkillName());
-	        preparedStatement.executeUpdate();
+			String sql = "insert into skills (skill_id, skill_name) values (?, ?)";
+			connectionUtility.setPreparedStatement(connectionUtility.getConnection().prepareStatement(sql));
+			connectionUtility.getPreparedStatement().setInt(1, t.getSkillId());
+			connectionUtility.getPreparedStatement().setString(2, t.getSkillName());
+			connectionUtility.getPreparedStatement().executeUpdate();
             System.out.println("Successfully added!");
-		} catch (ClassNotFoundException | SQLException e){
-			System.out.println("Bug!");
-		} finally {
-			try {
-				if(connection != null) connection.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(preparedStatement != null) preparedStatement.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(resultSet != null) resultSet.close();
-			} catch (SQLException e) {e.printStackTrace();}
-		}
+		} catch (SQLException e){e.printStackTrace();
+		} finally {connectionUtility.closeConnection();}
 	}
 	
 	@Override
 	public Skill get(int id) {
 		Skill s = new Skill();
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
 		try{
-			Class.forName(JDBC_DRIVER);
-			connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
-			String sql = "select m2.skills.skill_id, m2.skills.skill_name "
-						+ "from m2.skills "
+			String sql = "select skills.skill_id, skills.skill_name "
+						+ "from skills "
 						+ "where skill_id = ?";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setInt(1, id);
-			resultSet = preparedStatement.executeQuery();
-			while (resultSet.next()) {
-				s.setSkillId(resultSet.getInt("skill_id"));
-				s.setSkillName(resultSet.getString("skill_name"));
+			connectionUtility.setPreparedStatement(connectionUtility.getConnection().prepareStatement(sql));
+			connectionUtility.getPreparedStatement().setInt(1, id);
+			connectionUtility.setResultSet(connectionUtility.getPreparedStatement().executeQuery());
+			while (connectionUtility.getResultSet().next()) {
+				s.setSkillId(connectionUtility.getResultSet().getInt("skill_id"));
+				s.setSkillName(connectionUtility.getResultSet().getString("skill_name"));
 			}
-		} catch (ClassNotFoundException | SQLException e){
-			e.printStackTrace();
-			System.out.println("Bug!");
-		} finally {
-			try {
-				if(connection != null) connection.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(preparedStatement != null) preparedStatement.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(resultSet != null) resultSet.close();
-			} catch (SQLException e) {e.printStackTrace();}
-		}
+		} catch (SQLException e){e.printStackTrace();
+		} finally {connectionUtility.closeConnection();}
 		return s;
 	}
 	
 	@Override
 	public void update(Skill t) {
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
 		try{
-			Class.forName(JDBC_DRIVER);
-			connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
-			String sql = "update m2.skills set skill_name=? where skill_id=?";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setString(1, "pHp");
-			preparedStatement.setInt(2, t.getSkillId());
-            preparedStatement.executeUpdate();
+			String sql = "update skills set skill_name=? where skill_id=?";
+			connectionUtility.setPreparedStatement(connectionUtility.getConnection().prepareStatement(sql));
+			connectionUtility.getPreparedStatement().setString(1, "pHp_");
+			connectionUtility.getPreparedStatement().setInt(2, t.getSkillId());
+			connectionUtility.getPreparedStatement().executeUpdate();
             System.out.println("Seccessfully updated!");
-		} catch (ClassNotFoundException | SQLException e){
-			e.printStackTrace();
-			System.out.println("Bug!");
-		} finally {
-			try {
-				if(connection != null) connection.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(preparedStatement != null) preparedStatement.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(resultSet != null) resultSet.close();
-			} catch (SQLException e) {e.printStackTrace();}
-		}		
+		} catch (SQLException e){e.printStackTrace();
+		} finally {connectionUtility.closeConnection();}		
 	}
 	
 	@Override
 	public void delete(int id) {
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
 		try{
-			Class.forName(JDBC_DRIVER);
-			connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
-			String sql = "delete from m2.skills where skill_id = ?";
-			preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setInt(1, id);
-			preparedStatement.executeUpdate();
+			String sql = "delete from skills where skill_id = ?";
+			connectionUtility.setPreparedStatement(connectionUtility.getConnection().prepareStatement(sql));
+			connectionUtility.getPreparedStatement().setInt(1, id);
+			connectionUtility.getPreparedStatement().executeUpdate();
             System.out.println("Successfully deleted!");
-		} catch (ClassNotFoundException | SQLException e){
-			e.printStackTrace();
-			System.out.println("Bug!");
-		} finally {
-			try {
-				if(connection != null) connection.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(preparedStatement != null) preparedStatement.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(resultSet != null) resultSet.close();
-			} catch (SQLException e) {e.printStackTrace();}
-		}
+		} catch (SQLException e){e.printStackTrace();
+		} finally {connectionUtility.closeConnection();}	
 	}
 	
 	@Override
@@ -141,40 +67,23 @@ class SkillDAOImpl implements SkillDAO {
 		Skill s;
 		ArrayList<Skill> list = new ArrayList<>();
 		list.clear();
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
 		try{
-			Class.forName(JDBC_DRIVER);
-			connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
-			String sql = "select m2.developers.developer_first_name, m2.skills.skill_id, m2.skills.skill_name "
-						+ "from m2.developers "
-						+ "inner join m2.developers_skills on m2.developers_skills.dev_id = m2.developers.developer_id "
-						+ "inner join m2.skills on m2.developers_skills.ski_id = m2.skills.skill_id "
+			String sql = "select developers.developer_first_name, skills.skill_id, skills.skill_name "
+						+ "from developers "
+						+ "inner join developers_skills on developers_skills.dev_id = developers.developer_id "
+						+ "inner join skills on developers_skills.ski_id = skills.skill_id "
 						+ "where developer_first_name = ?";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setString(1, name);
-			resultSet = preparedStatement.executeQuery();
-			while (resultSet.next()) {
+			connectionUtility.setPreparedStatement(connectionUtility.getConnection().prepareStatement(sql));
+			connectionUtility.getPreparedStatement().setString(1, name);
+			connectionUtility.setResultSet(connectionUtility.getPreparedStatement().executeQuery());
+			while (connectionUtility.getResultSet().next()) {
 				s = new Skill();
-				s.setSkillId(resultSet.getInt("skill_id"));
-				s.setSkillName(resultSet.getString("skill_name"));
+				s.setSkillId(connectionUtility.getResultSet().getInt("skill_id"));
+				s.setSkillName(connectionUtility.getResultSet().getString("skill_name"));
 				list.add(s);
 			}
-		} catch (ClassNotFoundException | SQLException e){
-			e.printStackTrace();
-			System.out.println("Bug!");
-		} finally {
-			try {
-				if(connection != null) connection.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(preparedStatement != null) preparedStatement.close();
-			} catch (SQLException e) {e.printStackTrace();}
-			try {
-				if(resultSet != null) resultSet.close();
-			} catch (SQLException e) {e.printStackTrace();}
-		}
+		} catch (SQLException e){e.printStackTrace();
+		} finally {connectionUtility.closeConnection();}
 		return list;
 	}
 }
